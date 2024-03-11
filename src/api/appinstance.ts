01b74f298066a04c783921d046d7f53cd417d465
ShuangxueWu <qingchoulove@hotmail.com>
2024-03-11 19:02:34 +0800

    feat: replace server host
    Signed-off-by: ShuangxueWu <qingchoulove@hotmail.com>

--- src/api/appinstance.ts
+++ src/api/appinstance.ts
@@ -1,26 +1,29 @@
 import { requests } from "../util";
 import { AppInstance } from "../types/appinstance";
 import { Response } from "../types/common";
+import { server } from "../storage";
 
 async function listAllAppInstances(): Promise<Response<AppInstance[]>> {
-  return requests.get<AppInstance[]>("/api/v1/apps/instances");
+  return requests.get<AppInstance[]>(`${server}/api/v1/apps/instances`);
 }
 
 async function getAppInstance(
   instanceName: string
 ): Promise<Response<AppInstance>> {
-  return requests.get<AppInstance>(`/api/v1/apps/instances/${instanceName}`);
+  return requests.get<AppInstance>(
+    `${server}/api/v1/apps/instances/${instanceName}`
+  );
 }
 
 async function createOrUpdateAppInstance(appinstance: AppInstance) {
   return requests.post(
-    `/api/v1/apps/instances/${appinstance.metadata?.name}`,
+    `${server}/api/v1/apps/instances/${appinstance.metadata?.name}`,
     appinstance
   );
 }
 
 async function deleteAppInstance(instanceName: string) {
-  return requests.del(`/api/v1/apps/instances/${instanceName}`);
+  return requests.del(`${server}/api/v1/apps/instances/${instanceName}`);
 }
 
 export const appInstance = {
--- src/api/apptemplate.ts
+++ src/api/apptemplate.ts
@@ -1,13 +1,14 @@
+import { server } from "../storage";
 import { AppTemplate } from "../types/apptemplate";
 import { Response } from "../types/common";
 import { requests } from "../util";
 
 async function listAllAppTemplates(): Promise<Response<AppTemplate[]>> {
-  return requests.get<AppTemplate[]>("/api/v1/apps/templates");
+  return requests.get<AppTemplate[]>(`${server}/api/v1/apps/templates`);
 }
 
 async function getAppTemplate(name: string): Promise<Response<AppTemplate>> {
-  return requests.get<AppTemplate>(`/api/v1/apps/templates/${name}`);
+  return requests.get<AppTemplate>(`${server}/api/v1/apps/templates/${name}`);
 }
 
 export const appTemplate = {
