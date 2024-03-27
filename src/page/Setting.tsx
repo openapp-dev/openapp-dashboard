dc6353ba90cded9ec611ed90104072a2e81b70e8
ShuangxueWu <qingchoulove@hotmail.com>
2024-03-27 19:23:06 +0800

    cleanup: adjust setting page style
    Signed-off-by: ShuangxueWu <qingchoulove@hotmail.com>

--- src/page/Setting.tsx
+++ src/page/Setting.tsx
@@ -8,7 +8,7 @@ import { token } from "../storage";
 import { useAuth } from "../component/AuthProvider";
 
 interface State {
-  openappConfig?: OpenAPPConfig;
+  openappConfig: OpenAPPConfig;
   loading: boolean;
   error: string | null;
 }
@@ -17,10 +17,12 @@ export default function SettingPage() {
   const [state, setState] = useState<State>({
     loading: true,
     error: null,
+    openappConfig: {
+      username: "",
+      password: "",
+      registry: "",
+    },
   });
-  const [userName, setUserName] = useState<string>("");
-  const [userPassword, setUserPassword] = useState<string>("");
-  const [registries, setRegistries] = useState<string>("");
 
   useEffect(() => {
     async function fetchData() {
@@ -30,20 +32,39 @@ export default function SettingPage() {
         return;
       }
       setState({
-        openappConfig: data ?? undefined,
+        openappConfig: data ?? state.openappConfig,
         loading: false,
         error: null,
       });
     }
-    setUserName(state.openappConfig?.username?? "");
-    setUserPassword(state.openappConfig?.password?? "");
-    setRegistries(state.openappConfig?.registry?? "");
     fetchData();
   }, []);
 
   useEffect(() => {
     // TODO: send notification
   }, [state.error]);
+
+  function handleFormChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      openappConfig: {
+        ...state.openappConfig,
+        [name]: value,
+      },
+    });
+  }
+
+  async function handleFormSubmit() {
+    const openappConfig = state.openappConfig;
+    const resp = await setting.updateSetting(openappConfig);
+    if (!resp.success) {
+      setState({ ...state, error: resp.message });
+      return;
+    }
+  }
 
   const navigate = useNavigate();
   const auth = useAuth();
@@ -62,7 +83,7 @@ export default function SettingPage() {
         </span>
       </div>
       <Divider />
-      <div className="flex flex-col p-4 space-y-4 w-3/4 justify-center">
+      <div className="flex flex-col p-4 space-y-4 w-full justify-center">
         <div className="flex md:space-x-4 md:flex-row flex-col space-y-2 justify-center">
           <div className="flex-none w-24 h-24 rounded-md p-2 flex items-center justify-center">
             <img src={logo} />
@@ -72,46 +93,48 @@ export default function SettingPage() {
           <div className="flex flex-col p-2 space-y-2 mt-10 justify-center">
             <div className="flex items-center flex-col sm:flex-row sm:space-x-4 space-y-2 justify-center">
               <label className="sm:min-w-32">User Name</label>
-              <input
+              <Input
+                name="username"
+                size="md"
+                className="w-full focus:outline-blue-500"
                 type="text"
-                id="name-input"
-                className="block w-full p-2 text-gray-400 border border-gray-300 rounded-lg bg-white text-base focus:ring-blue-500 focus:border-blue-500"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
+                value={state.openappConfig.username}
+                onChange={handleFormChange}
               />
             </div>
             <div className="flex items-center flex-col sm:flex-row sm:space-x-4 space-y-2">
               <label className="sm:min-w-32">User Password</label>
-              <input
-                type="text"
-                id="password-input"
-                className="block w-full p-2 text-gray-400 border border-gray-300 rounded-lg bg-white text-base focus:ring-blue-500 focus:border-blue-500"
-                value={userPassword}
-                onChange={(e) => setUserPassword(e.target.value)}
+              <Input
+                name="password"
+                size="md"
+                className="w-full focus:outline-blue-500"
+                type="password"
+                value={state.openappConfig.password}
+                onChange={handleFormChange}
               />
             </div>
             <div className="flex items-center flex-col sm:flex-row sm:space-x-4 space-y-2">
               <label className="sm:min-w-32">Registry</label>
-              <textarea
-                id="registries"
+              <Textarea
+                name="registry"
+                className="w-full focus:outline-blue-500"
                 rows={6}
-                className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Input your custom registries..."
-                value={registries}
-                onChange={(e) => setRegistries(e.target.value)}
+                value={state.openappConfig.registry}
+                onChange={handleFormChange}
               />
             </div>
-            <div className="flex justify-center">
+            <div className="flex justify-center mt-20 space-x-10">
               <Button
                 wide
-                className="rounded-xl -mb-10 border-blue-500 color-white text-blue-500 bg-white hover:bg-blue-200 mr-10 mt-20"
+                className="rounded-xl -mb-10 border-blue-500 color-white text-blue-500 bg-white hover:bg-blue-200 "
                 onClick={handleLogout}
               >
                 Sign Out
               </Button>
               <Button
                 wide
-                className="rounded-xl -mb-10 border-none color-white text-white bg-sky-600 hover:bg-sky-700 ml-10 mt-20"
+                className="rounded-xl -mb-10 border-none color-white text-white bg-sky-600 hover:bg-sky-700"
+                onClick={handleFormSubmit}
               >
                 Update
               </Button>
