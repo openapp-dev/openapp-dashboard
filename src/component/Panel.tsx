027b99b9bcb2c0d6a1ac108c657be58ca4424ad4
richardli1598 <opennaslab@gmail.com>
2024-03-30 21:49:21 +0800

    feat: adjust app template page
    Signed-off-by: richardli1598 <opennaslab@gmail.com>

--- src/component/Panel.tsx
+++ src/component/Panel.tsx
@@ -7,8 +7,8 @@ interface PanelProps {
 
 export default function Panel({ title, children }: PanelProps) {
   return (
-    <div className="flex flex-col border border-gray-300 rounded-lg overflow-hidden">
-      <div className="bg-base-300 px-4 py-2">{title}</div>
+    <div className="flex flex-col border border-gray-300 rounded-lg">
+      <div className="bg-base-300 px-4 py-2 rounded-t-lg">{title}</div>
       {children}
     </div>
   );
