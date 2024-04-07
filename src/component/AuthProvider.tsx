9327c981d4ef1589a7e44b91b6a19a30625cc91d
ShuangxueWu <qingchoulove@hotmail.com>
2024-04-07 19:22:34 +0800

    feat: Unauthorized redirect to login page.
    Signed-off-by: ShuangxueWu <qingchoulove@hotmail.com>

--- src/component/AuthProvider.tsx
+++ src/component/AuthProvider.tsx
@@ -3,31 +3,27 @@ import { Navigate, useLocation } from "react-router-dom";
 import { token } from "../storage";
 
 interface AuthContextType {
-  user: string;
   isAuthenticated: boolean;
-  signin: (user: string, callback: VoidFunction) => void;
+  signin: (callback: VoidFunction) => void;
   signout: (callback: VoidFunction) => void;
 }
 
 const AuthContext = createContext<AuthContextType>(null!);
 
 function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState<string>(null!);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
     return token.get() !== null;
   });
 
-  const signin = (newUser: string, callback: VoidFunction) => {
-    setUser(newUser);
+  const signin = (callback: VoidFunction) => {
     setIsAuthenticated(true);
     callback();
   };
   const signout = (callback: VoidFunction) => {
-    setUser(null!);
     setIsAuthenticated(false);
     callback();
   };
-  const value = { user, isAuthenticated, signin, signout };
+  const value = { isAuthenticated, signin, signout };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
